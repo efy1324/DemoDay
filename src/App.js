74e3afa6f0fcc41cdb888f016c726784e3120b28
efy1324 <leviefraim@gmail.com>
2024-06-21 18:47:15 +0300

    demo 3d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ImageButton from "./components/Imagebtn";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Выберите предмет мебели, который хотите увидеть в своём интерьере</h1>
       </header>
+      <div className="imageCon">
+        <ImageButton />
+      </div> 
     </div>
   );
 }
---
+++ src/components/Imagebtn.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+
+
+const ImageButton = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const images = [
+    'https://dax.co.il/wp-content/uploads/2020/06/derby.jpg',
+    'https://dax.co.il/wp-content/uploads/2022/05/cupido0360.jpg',
+    'https://dax.co.il/wp-content/uploads/2018/06/13-1.jpg'
+  ];
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  return (
+    <div className='imagesBox'>
+     
+            <a href="https://annaleites.github.io/MyRoom/" target="_blank" rel="noopener noreferrer"> 
+        <img src="https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/blue_repicthousebase_1484336386-1.png" alt="description"/>
+        <div>Моя комната</div>
+      </a>
+            <a href="https://annaleites.github.io/Derby/" target="_blank" rel="noopener noreferrer" className='imageLink'>
+        <img src="https://dax.co.il/wp-content/uploads/2020/06/derby.jpg" alt="description"  />
+        <div>Стул Derby</div>
+      </a>
+            <a href="https://annaleites.github.io/Cupido/" target="_blank" rel="noopener noreferrer">
+        <img src="https://dax.co.il/wp-content/uploads/2022/05/cupido0360.jpg" alt="description"  />
+        <div>Кресло Cupido</div>
+      </a>
+            <a href="https://annaleites.github.io/Vicenza/" target="_blank" rel="noopener noreferrer">
+        <img src="https://dax.co.il/wp-content/uploads/2018/06/13-1.jpg" alt="description"  />
+        <div>Комод Vicenza</div>
+      </a>
+    </div>
+  );
+};
+
+export default ImageButton;
+// import React, { useState } from 'react';
+// import './Imagebtn.css'; 
+
+// const ImageButton = () => {
+//   const [selectedImage, setSelectedImage] = useState(null);
+
+//   const images = [
+//     {
+//       src: 'https://dax.co.il/wp-content/uploads/2020/06/derby.jpg',
+//       alt: 'Derby',
+//       link: 'https://annaleites.github.io/Derby/',
+//       name: 'Derby'
+//     },
+//     {
+//       src: 'https://dax.co.il/wp-content/uploads/2022/05/cupido0360.jpg',
+//       alt: 'Cupido',
+//       link: 'https://annaleites.github.io/Cupido/',
+//       name: 'Cupido'
+//     },
+//     {
+//       src: 'https://dax.co.il/wp-content/uploads/2018/06/13-1.jpg',
+//       alt: 'Vicenza',
+//       link: 'https://annaleites.github.io/Vicenza/',
+//       name: 'Vicenza'
+//     }
+//   ];
+
+//   const handleImageClick = (image) => {
+//     setSelectedImage(image);
+//   };
+
+//   return (
+//     <div className='imagesBox'>
+//       <a href="https://annaleites.github.io/MyRoom/" target="_blank" rel="noopener noreferrer"> 
+//         <img src="https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/blue_repicthousebase_1484336386-1.png" alt="MyRoom" />
+//         <p>MyRoom</p>
+//       </a>
+//       {images.map((image, index) => (
+//         <a key={index} href={image.link} target="_blank" rel="noopener noreferrer" className='imageLink'>
+//           <img src={image.src} alt={image.alt} />
+//           <p>{image.name}</p>
+//         </a>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default ImageButton;
